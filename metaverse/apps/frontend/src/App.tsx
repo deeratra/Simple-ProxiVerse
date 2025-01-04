447006c20c95fdff43c6781ba002654b5791243c
deeratra <drattra@gmail.com>
2025-01-04 19:57:59 +0100

    Updating landing page

--- metaverse/apps/frontend/src/App.tsx
+++ metaverse/apps/frontend/src/App.tsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route path="/" element={isLoggedIn ? <Posts /> : <Signin setIsLoggedIn={setIsLoggedIn} />} /> */}
+        <Route path="/" element={<Register />} />
         <Route path="/signup" element={<Register />}></Route>
         <Route path="/signin" element={<Login />}></Route>
         <Route path="/home" element={<Home />}></Route>
